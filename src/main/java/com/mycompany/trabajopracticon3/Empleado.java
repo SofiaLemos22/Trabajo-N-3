1ec6d5030b3255eac080138e99722ac54dd8f1c4
Sofía Lemos <sofialemos091@gmail.com>
2025-04-16 03:31:32 +0100

    Se actualizo

--- src/main/java/com/mycompany/trabajopracticon3/Empleado.java
+++ src/main/java/com/mycompany/trabajopracticon3/Empleado.java
@@ -11,4 +11,51 @@ package com.mycompany.trabajopracticon3;
  */
 public class Empleado {
     
+    private int identificador;
+    private String nombre;
+    private String puesto;
+    private double salario;
+    static int totalEmpleados = 0;
+    
+   
+    
+    public Empleado(int identificador, String nombre, String puesto, double salario) {
+        totalEmpleados++;
+        this.identificador = identificador;
+        this.nombre = nombre;
+        this.puesto = puesto;
+        this.salario = salario;     
+    }
+
+ 
+    public Empleado(String nombre, String puesto) {
+        totalEmpleados++;
+        this.identificador = totalEmpleados + 1;
+        this.nombre = nombre;
+        this.puesto = puesto;
+        this.salario = 30000;       
+    }
+    
+    public void actualizarSalario(int porcentajeAumento){
+        this.salario += (this.salario * porcentajeAumento) / 100;        
+    }
+    
+    public void actualizarSalario(double aumento){
+        this.salario += aumento;
+    }
+    
+      
+    public static int mostrarTotalEmpleados(){
+        return totalEmpleados;
+    }
+    
+
+    @Override
+    public String toString() {
+        return "Empleado{" + "identificador=" + identificador + ", nombre=" + nombre + ", puesto=" + puesto + ", salario=" + salario + '}';
+    }
+    
+    
 }
+
+
--- src/main/java/com/mycompany/trabajopracticon3/TrabajoPracticoN3.java
+++ src/main/java/com/mycompany/trabajopracticon3/TrabajoPracticoN3.java
@@ -11,6 +11,35 @@ package com.mycompany.trabajopracticon3;
 public class TrabajoPracticoN3 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        
+    
+
+        
+        Empleado emp1 = new Empleado(1, "Sofía López", "Desarrolladora", 60000);
+        Empleado emp2 = new Empleado("Juan Pérez", "Diseñador");
+        Empleado emp3 = new Empleado("Lucía Gómez", "Tester");
+
+        emp1.actualizarSalario(5000);
+       
+        emp2.actualizarSalario(10);
+   
+        emp3.actualizarSalario(5);
+        
+        emp3.actualizarSalario(2000);
+
+ 
+        System.out.println(emp1);
+        System.out.println(emp2);
+        System.out.println(emp3);
+
+   
+        System.out.println("Total de empleados creados: " + Empleado.mostrarTotalEmpleados());
     }
 }
+
+        
+        
+        
+    
+
