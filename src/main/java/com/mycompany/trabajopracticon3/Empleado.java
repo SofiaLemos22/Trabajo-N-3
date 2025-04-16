c71d29746b7fe9aba4d5e22abfae9e752763714e
Sofía Lemos <sofialemos091@gmail.com>
2025-04-16 03:29:43 +0100

    Trabajo Practico N3

---
+++ src/main/java/com/mycompany/trabajopracticon3/Empleado.java
@@ -0,0 +1,14 @@
+package com.mycompany.trabajopracticon3;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Fran
+ */
+public class Empleado {
+    
+}
